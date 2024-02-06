9b838ec146ea2cb7891a01f249ca2f38fd34547b
Bartłomiej Lańczyk <bartlomiejl@backbase.com>
2024-02-06 17:29:07 +0100

    Initial Commit

---
+++ raywenderlich/raywenderlichApp.swift
@@ -0,0 +1,17 @@
+//
+//  raywenderlichApp.swift
+//  raywenderlich
+//
+//  Created by Bartlomiej Lanczyk on 06/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct raywenderlichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
